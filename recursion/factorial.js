c78a4e3abd4869b903b27b9bc879b4850b5a6f80
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-02-24 12:38:18 -0800

    factorial problem

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -16,3 +16,10 @@ function factorial(num){
   if( num === 1) return 1;
   return num * factorial(num-1)
 }
+
+
+function factorial(x){
+  if (x < 0 ) return 0;
+  if (x <= 1 ) return 1;
+  return x * factorial(x-1);
+}
