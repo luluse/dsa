9e2c4a21cf5f1d7cc0ad74ee0c8be3b6d70d9c88
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-03 12:36:05 -0800

    merge sort on two sorted arrays

---
+++ sort/mergeSort.js
@@ -0,0 +1,33 @@
+// combinaison of splitting up, merging and sorting
+
+// create an empty array tha you'll return at the end.
+// take a look at the smalles value in each input array
+// use while loops
+
+// Merges two already sorted arrays
+function merge(arr1, arr2){
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while(i < arr1.length && j < arr2.length){
+      if(arr2[j] > arr1[i]){
+          results.push(arr1[i]);
+          i++;
+      } else {
+          results.push(arr2[j])
+          j++;
+      }
+  }
+  while(i < arr1.length) {
+      results.push(arr1[i])
+      i++;
+  }
+  while(j < arr2.length) {
+      results.push(arr2[j])
+      j++;
+  }
+  return results;
+}
+merge([100,200], [1,2,3,5,6])
+
+                 
