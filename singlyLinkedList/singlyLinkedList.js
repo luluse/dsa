e75f935c55e424f82a90c9192ae44c637373d8e2
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-07 17:12:15 -0800

    pop singly linked list

--- singlyLinkedList/singlyLinkedList.js
+++ singlyLinkedList/singlyLinkedList.js
@@ -3,37 +3,68 @@
 // easy to and and move from the beginning
 // Good data structure for insertion and deletion
 
-// push pseudo code:
-// accepts a value, create a new node using the value passed ti the function. if there are no head property on the list, sned head and tail to be this new node. otherwise set the next propery on the tail to be the new node. increment length by one.
+// push pseudo code (add at the end of the list):
+// accepts a value, create a new node using the value passed to the function. 
+// if there are no head property on the list, set head and tail to be this new node. 
+// otherwise set the next propery on the tail to be the new node.
+// increment length by one.
+// return linked list
 
+// pop pseudo code (remove from the end of list):
+// if there is no node ( no head or length is 0) return undefined.
+// loop through until you reach the tail. 
+// set the next property of sencond to last node to be null. 
+// set tail to be 2nd to last node. 
+// decrement lenght of list by one. 
+// Return value of node removed
 
-class Node{
-  constructor(val){
-      this.val = val;
-      this.next = null;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-class SinglyLinkedList{
-  constructor(){
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
-  push(val){
-     var newNode = new Node(val);
-     
-     if(!this.head){
+  push(val) {
+    var newNode = new Node(val);
+
+    if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
-     } else {
-       this.tail.next = newNode;
-       this.tail = newNode;
-     }
-     this.length++;
-     return this;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
 
   }
+
+
 }
 
 
