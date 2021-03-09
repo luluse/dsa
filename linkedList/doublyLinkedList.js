4b995a7e5c1d5e52320842486fa0e45316ca25d1
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-08 16:42:01 -0800

    pop shift unshift doubly linked list

--- linkedList/doublyLinkedList.js
+++ linkedList/doublyLinkedList.js
@@ -9,26 +9,32 @@
 
 
 // pop pseudo code (remove from the end of list):
-// if there is no node ( no head or length is 0) return undefined.
-// loop through until you reach the tail. 
-// set the next property of sencond to last node to be null. 
-// set tail to be 2nd to last node. 
+// if there is no node ( no head, no tail or length is 0) return undefined.
+// store current tail in a var
+// if the length is 1, set the head and tail to be null
+// update the tail to be old tail.previous
+// make new tail.next null
+// set old tail.previous to be null
 // decrement lenght of list by one. 
 // Return value of node removed
 
 
 // shifting pseudo code (remove from the beginnig of list):
 // if there is no node, return undefined
-// else store current head in variable
-// set current head next to be head
+// store current head in variable
+// if the length is 1, set the head and tail to be null
+// set current head.next to be head
+// set new head previous property to null
+// set old head next property to null
 // decrement length by one
 // return value of removed node
 
 
 // unshift pseudo code (adding a new node to the beginnig of list):
-// accepts a value, create a nre node using the value passed to the function
+// accepts a value, create a new node using the value passed to the function
 // if there are no head property on the list, set head and tail to be this new node. 
-// otherwise set newly created node's next property to be the current head
+// otherwise set previous property of head to be new node; 
+// set next property of new node to be the current head
 // set head property to be that newly created node
 // increment length by one
 // return linked list
@@ -59,6 +65,51 @@ class DoublyLinkedList {
       this.tail.next = newNode;
       newNode.previous = this.tail;
       this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var popped = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+    this.tail = popped.previous;
+    this.tail.next = null;
+    popped.previous = null;
+    }
+    this.length--;
+    return popped;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+    this.head = oldHead.next;
+    this.head.previous = null;
+    oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    var newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.previous = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
     }
     this.length++;
     return this;
