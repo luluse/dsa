eaaa771b74b4061ec26f3d6671b7b0d21fd4ccd9
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-15 11:05:59 -0700

    intro to trees

---
+++ tree/binarySearchTree.js
@@ -0,0 +1,3 @@
+// sorted data - can be compared
+// excell at searching
+// have 0,1 or 2 children 
---
+++ tree/trees.js
@@ -0,0 +1,20 @@
+// a tree is a data structure that consist of nodes in a parent/child relationship
+// threes are nonlinear- we can take many different paths
+
+
+// root - top node in a tree
+// child - a node directly connected to another node when moving away from the root
+// parent - a node directly connected to another node when moving toward the root
+// siblings -  group of nodes with the same parent
+// leaf - node with no childen
+// edge - connection between one node and another
+
+
+// trees applications:
+// HTML DOM
+// Network routing
+// abstract syntax tree (way of describing syntax of  programming language)
+// AI & machine learning
+// folders in oparating systems
+
+
