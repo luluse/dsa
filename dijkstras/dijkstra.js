0c60038b87ba9f22155667fefdfef92028cfc486
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-03-22 11:17:53 -0700

    intro to dijkstra

---
+++ dijkstras/dijkstra.js
@@ -0,0 +1,22 @@
+// find the shortest path between two vertices on a graph
+
+// USES
+// GPS
+// network routing
+// airline tickets
+// biology- used to model the spread of viruses
+
+
+
+class WeightedGraph {
+  constructor() {
+      this.adjacencyList = {};
+  }
+  addVertex(vertex){
+      if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1,vertex2, weight){
+      this.adjacencyList[vertex1].push({node:vertex2,weight});
+      this.adjacencyList[vertex2].push({node:vertex1, weight});
+  }
+}
